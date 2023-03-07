834953349f642d7b1ee909078bd5414d1f501083
Konstantinos Tzallas <90097950+Tzalk1985@users.noreply.github.com>
2023-03-07 12:26:00 +0200

    Add files via upload

--- Javascript/adddeletetable.js
+++ Javascript/adddeletetable.js
@@ -6,7 +6,7 @@ function selectedRowToInput() {
 
     for (var i = 1; i < table.rows.length; i++) {
         table.rows[i].onclick = function () {
-            dmppIndex = this.rowIndex;
+            dmppIndex = this.rowIndex
             document.getElementById("DMPP").value = this.cells[0].innerHTML;
             document.getElementById("H3PO4").value = this.cells[1].innerHTML;
             document.getElementById("DMP").value = this.cells[2].innerHTML;
@@ -15,9 +15,22 @@ function selectedRowToInput() {
             document.getElementById("BLUE").value = this.cells[5].innerHTML;
             document.getElementById("Kilos").value = this.cells[6].innerHTML;
             document.getElementById("Litre").value = this.cells[7].innerHTML;
-
+        
+         {
+            // remove the background from the previous selected row
+           if(typeof index !== "undefined"){
+              table.rows[index].classList.toggle("selected");
+           }
+           // get the selected row index
+           index = this.rowIndex;
+           // add class selected to the row
+           this.classList.toggle("selected");
+           
+        };
+   }
         };
     }
-}
+
 selectedRowToInput();
+
 
